6bf25f2213278ee08082977e6c9ba09e1e413f7b
bimal <gist@bimal.org.np>
2017-05-22 05:07:20 -0600

    unit tests output correction

--- phpunit/tests/controllersTest.php
+++ phpunit/tests/controllersTest.php
@@ -13,47 +13,60 @@ class controllersTest extends TestCase
 
     public function testControllerName()
     {
+        $package_name = "office";
+        $controller_name = "notes";
 		$rn = new RoutingNamifier();
 		
-		$controller = $rn->controllerName("notes");
+		$controller = $rn->controllerName($package_name, $controller_name);
         
-		$this->assertEquals("NotesController", $controller);
+		$this->assertEquals("office\\NotesController", $controller);
     }
 	
 	public function testControllerNameDigitsRemoved()
     {
+        $package_name = "office";
+        $controller_name = "123notes";
+
 		$rn = new RoutingNamifier();
-		
-		$controller = $rn->controllerName("123notes");
+		$controller = $rn->controllerName($package_name, $controller_name);
         
-		$this->assertEquals("NotesController", $controller);
+		$this->assertEquals("office\\NotesController", $controller);
     }
 	
 	public function testControllerNameDigitsPreserved()
     {
+        $package_name = "office";
+        $controller_name = "notes678";
+
 		$rn = new RoutingNamifier();
 		
-		$controller = $rn->controllerName("notes678");
+		$controller = $rn->controllerName($package_name, $controller_name);
         
-		$this->assertEquals("Notes678Controller", $controller);
+		$this->assertEquals("office\\Notes678Controller", $controller);
     }
 
 	public function testControllerNameDigitsRemovedAndPreserved()
     {
+        $package_name = "office";
+        $controller_name = "123notes678";
+
 		$rn = new RoutingNamifier();
 		
-		$controller = $rn->controllerName("123notes678");
+		$controller = $rn->controllerName($package_name, $controller_name);
         
-		$this->assertEquals("Notes678Controller", $controller);
+		$this->assertEquals("office\\Notes678Controller", $controller);
     }
 	
 	public function testControllerSpecialCharactersRemoved()
     {
-		$rn = new RoutingNamifier();
+        $package_name = "office";
+        $controller_name = "my-notes";
+
+        $rn = new RoutingNamifier();
 		
 		// src/my-notes/add/
-		$controller = $rn->controllerName("my-notes");
+		$controller = $rn->controllerName($package_name, $controller_name);
         
-		$this->assertEquals("MynotesController", $controller);
+		$this->assertEquals("office\\MynotesController", $controller);
     }
 }
